39c8561bd0cdd1b1c71a64bf0fee6b6c9cddc789
William Ma <william.ma64@gmail.com>
2018-08-07 23:49:18 +0000

    forgot the php

--- mail.php
+++ mail.php
@@ -29,7 +29,7 @@ if ($message === ''){
   die();
 }
 $content="From: $name \nEmail: $email \nMessage: $message";
-$recipient = "youremail@here.com";
+$recipient = "william.ma64@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
 echo "Email sent!";
