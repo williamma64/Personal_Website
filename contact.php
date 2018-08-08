709e29e85ea8b41152eb3d77907d1014c815c4cb
williamma64 <william.ma64@gmail.com>
2018-08-07 17:07:24 -0700

    no longer need picture nor contact.html

--- contact.php
+++ contact.php
@@ -111,31 +111,37 @@
 		<script>new WOW().init();</script>
 
 		<script>
-		function validateForm() {
-		document.getElementByID('status').innerHTML = "Sending...";
-		formData = {
-			'name'		: $('input[name=name]').val(),
-			'email'		: $('input[name=email]').val(),
-			'subject'	: $('input[name=subject]').val(),
-			'message'	: $('input[textarea[name=message]').val()
-		};
 
-		$.ajax({
-			url: "mail.php",
-			type: "POST",
-			data: formData,
-			success: function(data, textStatus, jqXHR) {
-				$('#status').text(data.message);
-				if(data.code)
-				$('#contact-form').closest('form').find("input[type=text], textarea").val("");
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				$('#status').text(jqXHR);
-			}
-		});
+		function validateForm() {
+
+    	document.getElementById('status').innerHTML = "Sending...";
+    	formData = {
+        	'name'     : $('input[name=name]').val(),
+        	'email'    : $('input[name=email]').val(),
+        	'subject'  : $('input[name=subject]').val(),
+        	'message'  : $('textarea[name=message]').val()
+    	};
+
+
+   		$.ajax({
+    		url : "mail.php",
+    		type: "POST",
+    		data : formData,
+    		success: function(data, textStatus, jqXHR) {
+
+        	$('#status').text(data.message);
+        	if (data.code) //If mail was sent successfully, reset the form.
+        	$('#contact-form').closest('form').find("input[type=text], textarea").val("");
+    	},
+    	error: function (jqXHR, textStatus, errorThrown)
+    	{
+        	$('#status').text(jqXHR);
+    	}
+			});
 
 		}
-		</script>
+		
+    </script>
 		
 	</body>
 </html>
